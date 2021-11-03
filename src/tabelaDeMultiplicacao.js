1a39e30339216987c7f6ccc7b7eb224eec3a86cf
jlucas19 <jlucasnove@gmail.com>
2021-11-02 23:41:44 -0300

    Update tabelaDeMultiplicacao.js
    adiciona tabela

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,15 @@
-function nomeDaFuncao() { 
-    // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
-    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
-    }
-    console.table(x)
+function mul(num) {
+  let x = [];
+  for (let i = 0; i <= num; i++) {
+      x[i] = [[i * 0],[i * 1],[i * 2],[i * 3],[i * 4],[i * 5],[i * 6],[i * 7],[i * 8],[i * 9],[i * 10]]
+  }
+
+  console.table(x)
 }
+console.log(mul(10))
+
+/* código inacabado ainda (fiz somente com um loop)
+ Ao contrario do console do CMD
+ no console do navegador não se mostra os resultados corretos
+ [ mostra somente Array(1) ]
+*/
